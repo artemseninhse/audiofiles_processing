45fe78af324875a526213b69ccd41f7b603b373b
Artem <ASSenin@sberbank.ru>
2021-04-09 17:19:32 +0300

    fixed arguments

--- audio_converter.py
+++ audio_converter.py
@@ -3,37 +3,41 @@ import os
 from argparse import ArgumentParser
 from pydub import AudioSegment
 from os.path import join, exists
+from utils import CONVERTED_DIR
 
 
-def get_audio_paths(base_dir, frmt):
-    files_list = os.listdir(base_dir)
+def get_audio_paths(args):
+    files_list = os.listdir(args.base_dir)
+    frmt = "mp3" if args.mp3 else "wav"
     return [file for file in files_list
             if file.endswith(frmt)]
 
 
-def convert_mp3_to_wav(mp3_path, out_dir, wav_name):
-    audio = AudioSegment.from_mp3(mp3_path)
+def convert_mp3_to_wav(base_dir,
+                       mp3_path,
+                       wav_name):
+    audio = AudioSegment.from_mp3(join(base_dir, mp3_path))
+    out_dir = join(base_dir, CONVERTED_DIR)
     if not exists(out_dir):
         os.mkdir(out_dir)
     audio.export(os.path.join(out_dir, wav_name))
 
 
 def convert_batch(args):
-    audio_paths = get_audio_paths(args.base_dir,
-                                  args.frmt)
+    audio_paths = get_audio_paths(args)
     for mp3_name in audio_paths:
         wav_name = mp3_name.replace("mp3", "wav")
-        filepath = join(args.base_dir, mp3_name)
-        convert_mp3_to_wav(filepath, args.out_dir, wav_name)
+        convert_mp3_to_wav(args.base_dir,
+                           mp3_name,
+                           wav_name)
 
 
 parser = ArgumentParser()
-parser.add_argument("--frmt",
+parser.add_argument("--mp3",
                     required=False,
-                    default="mp3")
+                    default=False,
+                    action="store_true")
 parser.add_argument("--base_dir",
-                    required=True)
-parser.add_argument("--out_dir",
                     required=True)
 
 
