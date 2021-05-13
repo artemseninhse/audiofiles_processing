57eeb2215b6b24a403d6397fb2f14e9676c76830
Artem Senin <artemseninhse@gmail.com>
2021-05-13 13:27:21 +0000

    batch augmentation added

--- audio_transform.py
+++ audio_transform.py
@@ -13,21 +13,23 @@ from os.path import (
 from sox.transform import Transformer
 
 
-class AudioAug(Transformer):
+DEFAULT_ARGS = {
+            "contrast": [75],
+            "speed": [0.9],
+            "dcshift": [-1],
+            "pad": [1,0]
+        }
 
+
+class AudioAug:
+    
     def __init__(self, 
                  transform, 
                  params=[]):
-        super(AudioAug, self).__init__()
         self.transform = transform
         self.params = params
         if not self.params:
-            func = getattr(self, self.transform)
-            sig = signature(func)
-            self.params = {k: v.default
-                           for k, v in sig.parameters.items()
-                           if v.default is not Parameter.empty
-                           }
+            self.params = DEFAULT_ARGS[self.transform]
     
     def _get_out_path(self,
                       filename,
@@ -39,25 +41,30 @@ class AudioAug(Transformer):
     def transform_audio(self,
                         in_path,
                         out_path):
-        getattr(self, self.transform)(*self.params)
-        getattr(self, "build")(in_path, out_path)
-        arr = self.build_array(in_path)
+        tfm = Transformer()
+        getattr(tfm, self.transform)(*self.params)
+        getattr(tfm, "build")(in_path, out_path)
+        arr = tfm.build_array(in_path)
 
     def apply(self, 
               input_path, 
               out_dir):
+        if not exists(out_dir):
+            os.mkdir(out_dir)
         if isdir(input_path):
             for filename in os.listdir(input_path):
                 in_path = join(input_path,
                                 filename)
                 out_path = self._get_out_path(filename,
                                               out_dir)
+                print(out_path, input_path)
                 self.transform_audio(in_path,
                                      out_path)
         else:
             filename = input_path.split("/")[-1]
             out_path = self._get_out_path(filename,
                                           out_dir)
+            print(out_path, input_path)
             self.transform_audio(input_path,
                                  out_path)
 
@@ -77,8 +84,6 @@ parser.add_argument("--params",
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    if not exists(args.out_dir):
-        os.mkdir(args.out_dir)
     augmentor = AudioAug(args.transform,
                          [float(i) for i in args.params])
     augmentor.apply(args.in_path,
